05d3de6423c00c1e30947fbf24be5c9b4b1537bd
Jaume Barrios <jaume.barrios@cuatroochenta.com>
2026-02-10 18:00:47 +0100

    fix: redundant list code

--- internal/tui/screens/skills/view.go
+++ internal/tui/screens/skills/view.go
@@ -1,13 +1,6 @@
 package skills
 
-import (
-	"fmt"
-)
-
 func (s SkillsScreen) View() string {
-	originalTitle := s.List.Title
-	s.List.Title = fmt.Sprintf("%s (Pág. %d/%d)", originalTitle, s.List.Paginator.Page+1, s.List.Paginator.TotalPages)
 	view := s.List.View()
-	s.List.Title = originalTitle
 	return view
 }
